666015f8db8ede02e66b8dcb956ee748efcef177
Anirban-01 <areyaban@gmail.com>
2018-09-24 04:43:20 -0700

    Add files via upload

---
+++ pythonfile.py
@@ -0,0 +1,23 @@
+s = "AnirbanSen"
+
+print(s[:1])
+print(s[:2])
+print(s[:3])
+print(s[:4])
+print(s[:5])
+print(s[:6])
+print(s[:7])
+print(s[:8])
+print(s[:9])
+print(s[:10])
+
+print(s[1:])
+print(s[2:])
+print(s[3:])
+print(s[4:])
+print(s[5:])
+print(s[6:])
+print(s[7:])
+print(s[8:])
+print(s[9:])
+print(s[10:])
